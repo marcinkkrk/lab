fac959780372db5f7daef4f279e2be27f3dfe32f
Marcin Kopański <azreak1234@gmail.com>
2025-07-14 18:10:40 +0200

    create cluster and helm provider

--- terraform/k8s/0-locals.tf
+++ terraform/k8s/0-locals.tf
@@ -1,8 +1,8 @@
 locals {
   env         = "dev"
   region      = "eu-west-1"
-  zone1       = "euw1-az1"
-  zone2       = "euw1-az2"
+  zone1       = "eu-west-1a"
+  zone2       = "eu-west-1b"
   eks_name    = "demo"
   eks_version = "1.32"
 }
---
+++ terraform/k8s/8-nodes.tf
@@ -0,0 +1,78 @@
+resource "aws_iam_role" "nodes" {
+  name = "${local.env}-${local.eks_name}-eks-nodes"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      }
+    }
+  ]
+}
+POLICY
+}
+
+# Policy that includes AssumeRoleForPodIdentity for the Pod Identity Agent
+resource "aws_iam_role_policy_attachment" "amazon_eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.nodes.name
+}
+
+# Policy to manage secondary IP's for the pods
+#A secondary IP is an extra IP address that is given to a pod, but it's not the main IP of the machine (node).
+# Some cloud platforms (like AWS, GCP, Azure) use secondary IPs to give pods their own IP addresses.
+# Better network isolation, Easier traffic control and monitoring, Pods to behave like real machines on the network
+resource "aws_iam_role_policy_attachment" "amazon_eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_ec2_container_registry_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes.name
+}
+
+# Behind the scenes its ec2 auto scaling group
+resource "aws_eks_node_group" "general" {
+  cluster_name    = aws_eks_cluster.eks.name
+  version         = local.eks_version
+  node_group_name = "general"
+  node_role_arn   = aws_iam_role.nodes.arn
+
+  subnet_ids = [
+    aws_subnet.private_zone1.id,
+    aws_subnet.private_zone2.id
+  ]
+
+  capacity_type  = "ON_DEMAND" # SPOT is cheaper
+  instance_types = ["t3.large"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 10
+    min_size     = 0
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  labels = {
+    role = "general"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.amazon_eks_worker_node_policy,
+    aws_iam_role_policy_attachment.amazon_eks_cni_policy,
+  ]
+
+  # Allow external changes without Terraform plan difference
+  lifecycle {
+    ignore_changes = [scaling_config[0].desired_size]
+  }
+}
